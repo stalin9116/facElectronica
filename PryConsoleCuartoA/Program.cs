99c279896cfde8eae272ddd20c89053e62f6efcb
Stalin <it@PC-SMEJIA.localdomain>
2020-11-18 21:27:34 -0500

    Creacion Clase Vehiculo

--- PryConsoleCuartoA/Program.cs
+++ PryConsoleCuartoA/Program.cs
@@ -10,6 +10,57 @@ namespace PryConsoleCuartoA
     {
         static void Main(string[] args)
         {
+            try
+            {
+                List<Vehiculo> _listaVehiculos = new List<Vehiculo>();
+
+                Vehiculo vehiculo = new Vehiculo();
+                vehiculo.Codigo = 1;
+                vehiculo.Placa = "PCW5425";
+                vehiculo.Color = "Gris";
+                vehiculo.Modelo = "Audi";
+                vehiculo.FechaMatricula = DateTime.Now;
+                vehiculo.Estado = "A";
+
+                _listaVehiculos.Add(vehiculo);
+
+                vehiculo = new Vehiculo();
+                //vehiculo.Codigo = 2;
+                //vehiculo.Placa = "PTR6570";
+                //vehiculo.Color = "Amarillo";
+                //vehiculo.Modelo = "Chevrolet";
+                //vehiculo.FechaMatricula = Convert.ToDateTime("2020-11-18");
+                //vehiculo.Estado = "I";
+
+                Console.WriteLine("Ingrese codigo ");
+                vehiculo.Codigo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese Placa ");
+                vehiculo.Placa = Console.ReadLine();
+                Console.WriteLine("Ingrese Color ");
+                vehiculo.Color = Console.ReadLine();
+                Console.WriteLine("Ingrese Modelo ");
+                vehiculo.Modelo = Console.ReadLine();
+                Console.WriteLine("Ingrese Estado ");
+                vehiculo.Estado = Console.ReadLine();
+
+                _listaVehiculos.Add(vehiculo);
+
+                foreach (var item in _listaVehiculos.Where(data=>data.Estado.Equals("A")))
+                {
+                    Console.WriteLine("Codigo: " + item.Codigo);
+                    Console.WriteLine("Placa: " + item.Placa);
+                    Console.WriteLine("Color: " + item.Color);
+                    Console.WriteLine("Modelo: " + item.Modelo);
+                }
+
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message);
+                //throw new ArgumentException("Error "+ ex.Message);
+            }
         }
     }
 }
---
+++ PryConsoleCuartoA/Vehiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryConsoleCuartoA
+{
+    public class Vehiculo
+    {
+        public int Codigo { get; set; }
+        public string Placa { get; set; }
+        public string Chasis { get; set; }
+        public string Color { get; set; }
+        public string Modelo { get; set; }
+        public DateTime FechaMatricula { get; set; }
+        public string Estado { get; set; }
+
+        //Metodo Constructor
+        public Vehiculo()
+        {
+
+        }
+
+    }
+}
